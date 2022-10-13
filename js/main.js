94623870af648ffcbd0c2c3abe4251656a670ec8
ri-ca4 <riley.de12@gmail.com>
2022-10-13 16:08:26 -0400

    fix getNewJob function

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     author: ri-ca4
 */
 
-window.load(main())
+$(main())
 
 var jobArray = [];
 var storageKey = 'myJobList';
@@ -44,9 +44,26 @@ function main() {
 
 //function to create job objects, add to jobArray, send to storage
 function getNewJob() {
+    
+    var title           = $('#jobTitle');
+    var company         = $('#company');
+    var apply           = $('input[name="apply"]:checked');
+    var applyDate       = $('#dateApplied');
+    var interview       = $('input[name="interview"]:checked');
+    var interviewDate   = $('#dateInterview');
+    var offer           = $('input[name="offer"]:checked');
+    var notes           = $('#notes');
 
     //make user input into an object
-    let jobObject = new Object();
+    let jobObject           = new Object();
+    jobObject.title         = $(title).val();
+    jobObject.company       = $(company).val();
+    jobObject.apply         = $(apply).val();
+    jobObject.applyDate     = $(applyDate).val();
+    jobObject.interview     = $(interview).val();
+    jobObject.interviewDate = $(interviewDate).val();
+    jobObject.offer         = $(offer).val();
+    jobObject.notes         = $(notes).val();
 
     //add task to array
     jobArray.push(jobObject);
@@ -55,6 +72,11 @@ function getNewJob() {
     localStorage.setItem(storageKey, JSON.stringify(jobArray));
 
     //reset input fields
+    $(title).val('')
+    $(company).val('')
+    $(applyDate).val('')
+    $(interviewDate).val('')
+    $(notes).val('')
 
     displayJobs();
 }
@@ -70,16 +92,52 @@ function removeJob() {
     displayJobs();
 };
 
+//function to "edit" job objects
+
 //function to display job list
 function displayJobs() {
+    console.log('called')
     jobArray = JSON.parse(localStorage.getItem(storageKey)); //get data and parse it to array
-    var myString = '';
+    console.log (jobArray)
+    var myString
         if (jobArray.length == 0){//if there is no data
-
+            alert('You have no current tasks!');
+            $('#inputSection').show();
+            $('#displaySection').hide();
         }else{
+            $('#inputSection').hide();
+            $('#displaySection').show();
             for (var i = 0; i < jobArray.length; i++) { 
+                var jobTitle         = jobArray[i].title;
+                var jobCompany       = jobArray[i].company;
+                var jobApply         = jobArray[i].apply;
+                var jobApplyDate     = jobArray[i].applyDate;
+                var jobInterview     = jobArray[i].interview;
+                var jobInterviewDate = jobArray[i].interviewDate;
+                var jobOffer         = jobArray[i].offer;
+                var jobNote          = jobArray[i].note;
+                var jobIndex         = i;
 
+                var string =
+                    `<tr class="job-item" id="job${jobIndex}">
+                        <td class="job-title">${jobTitle}</td>
+                        <td class="job-company">${jobCompany}</td>
+                        <td class="job-apply">${jobApply}</td>
+                        <td class="job-apply-date">${jobApplyDate}</td>
+                        <td class="job-interview">${jobInterview}</td>
+                        <td class="job-interview-date">${jobInterviewDate}</td>
+                        <td class="job-offer">${jobOffer}</td>
+                        <td class="job-note">${jobNote}</td>
+                        <td class="job-btns">
+                            <button class="job-edit" data-index="${jobIndex}">Edit</button>
+                            <button class="job-del" data-index="${jobIndex}">Delete</button>
+                        </td>
+                    </tr>`
+
+                myString += string;
             };
+
+            $('#jobTable').html(myString);
 
         };
 };
@@ -87,3 +145,7 @@ function displayJobs() {
 //functions to sort list
 
 //event listeners
+$('#submitJob').on('click', function(){
+    console.log('click')
+    getNewJob()
+})
