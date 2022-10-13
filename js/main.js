2792e60311a1d4a110978925398c7b3ec9b9f223
ri-ca4 <riley.de12@gmail.com>
2022-10-13 12:39:07 -0400

    fix form, add select section, outline js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-alert('connected')
+// alert('connected')
+
+/*
+    title: job search organizer
+    date: 10/13/22
+    author: ri-ca4
+*/
+
+var jobArray = [];
+var storageKey = 'myJobList';
+
+//function to test local storage
+
+//function to get job list from storage
+
+//function to create job objects, add to jobArray, send to storage
+
+//function to delete job objects
+
+//function to display job list
+
+//functions to sort list
+
+//event listeners
